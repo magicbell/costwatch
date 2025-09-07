75eb012c4d948c623310e4cbbc750fa7f44f3792
Stephan Meijer <stephan.meijer@gmail.com>
2025-09-07 11:35:04 +0200

    chore: config dev env

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -5,4 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tanstackRouter({ autoCodeSplitting: true }), viteReact(), tsconfigPaths()],
+  server: {
+    port: parseInt(process.env.PORT || "3000"),
+  },
 });
